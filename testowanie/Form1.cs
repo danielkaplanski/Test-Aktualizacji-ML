9b8a5ac4a359a853ee322814257ffba170127d25
danielkaplanski <danielkaplanski11@gmail.com>
2025-09-26 20:49:02 +0200

    33

--- testowanie/Form1.cs
+++ testowanie/Form1.cs
@@ -45,7 +45,6 @@ namespace testowanie
                 lblUpdateInfo.Text = $"B³¹d sprawdzania wersji: {ex.Message}";
             }
         }
-        //
         // 4. Pobierz wersjê z pliku version.txt z Netlify
         private async Task<string> GetRemoteVersionAsync()
         {
