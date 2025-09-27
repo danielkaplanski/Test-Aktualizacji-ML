46f4df44e69042b98ea1f45b01c3d1d4d0a16b46
danielkaplanski <danielkaplanski11@gmail.com>
2025-09-27 13:24:24 +0200

    Update Form1.Designer.cs

--- testowanie/Form1.Designer.cs
+++ testowanie/Form1.Designer.cs
@@ -67,9 +67,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(240, 63);
             label1.Name = "label1";
-            label1.Size = new Size(102, 15);
+            label1.Size = new Size(32, 15);
             label1.TabIndex = 3;
-            label1.Text = "Najnowsza Wersja";
+            label1.Text = "test1";
             // 
             // Form1
             // 
